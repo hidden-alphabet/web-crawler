35c2d0a66bffa2744f28225de40bd9edc5558431
Cole Hudson <cole@colejhudson.com>
2019-06-22 22:04:12 -0700

    initial commit

---
+++ upload.go
@@ -0,0 +1,47 @@
+package s3puttask
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/lambda-labs-13-stock-price-2/task-scheduler"
+	"os"
+)
+
+type S3PutJob struct {
+	Region string
+	Bucket string
+	Key    string
+	File   []byte
+}
+
+func S3PutWorker(j interface{}) *scheduler.Result {
+
+	output := &scheduler.Result{}
+
+	if job, ok := (j).(S3PutJob); !ok {
+		output.Err = errors.New("Coercion to S3PutJob failed.")
+		return output
+	} else {
+		sess := session.Must(session.NewSession(&aws.Config{Region: aws.String(job.Region)}))
+		uploader := s3manager.NewUploader(sess)
+
+		result, err := uploader.Upload(&s3manager.UploadInput{
+			Bucket: aws.String(job.Bucket),
+			Key:    aws.String(job.Key),
+			Body:   bytes.NewReader(job.File),
+		})
+
+		if err != nil {
+			output.Err = errors.New(fmt.Sprintf("Upload failed: %v", err))
+			return output
+		}
+
+		fmt.Println("Successfully uploaded to %s.", result.Location)
+
+		return output
+	}
+}
