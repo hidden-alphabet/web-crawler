63b3bf3b92b95fcdc8236e2c4816caecbd506418
Cole Hudson <cole@colejhudson.com>
2019-06-22 21:35:31 -0700

    removed comment

--- crawler/search.go
+++ crawler/search.go
@@ -15,8 +15,8 @@ const (
 	USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
 		"AppleWebKit/537.36 (KHTML, like Gecko) " +
 		"Chrome/74.0.3729.169 Safari/537.36"
-	BUCKET = "hidden-alphabet"                   // os.Getenv("AWS_S3_BUCKET")
-	KEY    = "datasets/webpages/raw/twitter.com" // os.Getenv("AWS_S3_KEY")
+	BUCKET = "hidden-alphabet"
+	KEY    = "datasets/webpages/raw/twitter.com"
 )
 
 type TwitterSearchJob struct {
